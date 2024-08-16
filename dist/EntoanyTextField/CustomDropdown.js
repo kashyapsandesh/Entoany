43144ff1ac83b6977d30d3a8d0b5746bf53a64bd
Sandesh Ghimire <88843968+kashyapsandesh@users.noreply.github.com>
2024-08-16 22:30:32 +0545

    revert to previous

--- dist/EntoanyTextField/CustomDropdown.js
+++ dist/EntoanyTextField/CustomDropdown.js
@@ -6,23 +6,24 @@ Object.defineProperty(exports, "__esModule", {
 exports["default"] = void 0;
 var _react = _interopRequireDefault(require("react"));
 require("./CustomDropdown.css");
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-// CustomDropdown.js
-
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
 // Import your CSS file for styling
 
 var CustomDropdown = function CustomDropdown(_ref) {
   var candidates = _ref.candidates,
     onSelectCandidate = _ref.onSelectCandidate;
+  var handleItemClick = function handleItemClick(candidate) {
+    onSelectCandidate(candidate); // Update parent state with selected candidate
+  };
   return /*#__PURE__*/_react["default"].createElement("ul", {
-    className: "dropdown-list"
+    className: "dropdown-list ".concat(candidates.length > 0 ? "open" : "")
   }, candidates.map(function (candidate, index) {
     return /*#__PURE__*/_react["default"].createElement("li", {
       key: index,
       className: "dropdown-item",
       onClick: function onClick() {
-        return onSelectCandidate(candidate);
-      }
+        return handleItemClick(candidate);
+      } // Call handler with candidate
     }, candidate);
   }));
 };
